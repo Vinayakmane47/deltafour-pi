f2d88be9c91ae0e1c63e9833546ea9db52a081ab
kamalesh pati <36737887+kamaleshpati@users.noreply.github.com>
2023-01-24 15:06:24 +0530

    Update main.go

--- main.go
+++ main.go
@@ -93,21 +93,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	go func() {
-		for {
-			buf := make([]byte, 1)
-			n := 0
-			n, err = s.Read(buf)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if n <= 0 {
-				fmt.Println("got 0")
-			}
-			fmt.Println(uint8(buf[0]))
-			// pi_channel <- read_int32_big(buf[:n])
+	for {
+		buf := make([]byte, 1)
+		n := 0
+		n, err = s.Read(buf)
+		if err != nil {
+			log.Fatal(err)
 		}
-	}()
+		if n <= 0 {
+			fmt.Println("got 0")
+		}
+		pi_channel <- uint8(buf[0])
+	}
 
 }
 
